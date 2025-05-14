e4e460d068e67fb3307d285d47486dc32277e19a
Crustwerk <omar.restuccia@live.it>
2025-05-14 15:45:13 +0200

    Creazione API, testata con Insomnia.

--- src/main/java/com/crustwerk/restapi/controller/UserController.java
+++ src/main/java/com/crustwerk/restapi/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.crustwerk.restapi.controller;
 
+import com.crustwerk.restapi.model.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Ciao Omar (o meglio... Crustwerk 😄), benvenuto nella tua prima API Spring Boot!";
+    }
+
+    @GetMapping("/sample")
+    public User getSampleUser() {
+        return new User("Omar", 30, 133.8);
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody User user) {
+        return "Utente ricevuto: " + user.getName() + ", età: " + user.getAge() + ", peso: " + user.getWeight();
+    }
 }
