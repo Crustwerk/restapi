e8a41e271e0249ffb9ddf24b675847f072c11507
Crustwerk <omar.restuccia@live.it>
2025-05-19 18:42:12 +0200

    Aggiunta configurazione DB (MySql in container Docker). Testare il funzionamento del db e fare un giro completo per fissare tutti i concetti finora trattati. Una volta chiusa la parte backend procedere a creare frontend che contatti l'API.

--- src/main/java/com/crustwerk/restapi/model/User.java
+++ src/main/java/com/crustwerk/restapi/model/User.java
@@ -1,9 +1,17 @@
 package com.crustwerk.restapi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDate;
 
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
